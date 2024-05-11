150eeeebdae6c66e4887af0d4de77053f3334682
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2024-05-11 21:14:16 +0300

    Add files via upload

---
+++ ipaudioplus-py3.12_4.1-r0_all.sh
@@ -0,0 +1,12 @@
+
+#!/bin/sh
+#
+
+wget -O /var/volatile/tmp/enigma2-plugin-extensions-ipaudioplus-py3.12_4.1-r0_all.ipk "https://raw.githubusercontent.com/Ham-ahmed/Novaler/main/enigma2-plugin-extensions-ipaudioplus-py3.12_4.1-r0_all.ipk"
+wait
+opkg install --force-overwrite /tmp/*.ipk
+wait
+rm -f /var/volatile/tmp/enigma2-plugin-extensions-ipaudioplus-py3.12_4.1-r0_all.ipk
+wait
+sleep 2;
+exit 0
