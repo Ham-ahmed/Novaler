b887edd7f6921f944a87c0b4b75234d9aad1d0dc
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2024-07-04 05:19:03 +0300

    Add files via upload

---
+++ novacampro-py3.12_3.0-r0.sh
@@ -0,0 +1,12 @@
+
+#!/bin/sh
+#
+
+wget -O /var/volatile/tmp/enigma2-plugin-extensions-novacampro-py3.12_3.0-r0_all.ipk "https://raw.githubusercontent.com/Ham-ahmed/Novaler/main/enigma2-plugin-extensions-novacampro-py3.12_3.0-r0_all.ipk"
+wait
+opkg install --force-overwrite /tmp/*.ipk
+wait
+rm -f /var/volatile/tmp/enigma2-plugin-extensions-novacampro-py3.12_3.0-r0_all.ipk
+wait
+sleep 2;
+exit 0
